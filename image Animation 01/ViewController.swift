f99205e222f97ad5574fe0f947891196b9ec8da7
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-03-27 14:48:18 +0900

    image Animation 01

--- image Animation 01/ViewController.swift
+++ image Animation 01/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alienImageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    var count = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alienImageView.image = UIImage(named: "frame\(count).png")
+        myLabel.text = "frame\(count).png"
     }
 
-
+    @IBAction func updateButtonPressed(_ sender: Any) {
+        // 누를때마다 카운트 1 증가
+        count += 1
+        if ( count > 5 ) {
+            count = 1
+        }
+        alienImageView.image = UIImage(named: "frame\(count).png")
+        myLabel.text = "frame\(count).png"
+    }
+    
+    
+    
+    
+    
+    
 }
 
